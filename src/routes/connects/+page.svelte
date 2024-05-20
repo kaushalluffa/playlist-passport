20c23c40ee33c049a42d36a281f5429c60cc5e6c
kaushalluffa <kaushalluffa91@gmail.com>
2024-05-20 15:03:46 -0400

    feat(dep): added a dep and connects page

---
+++ src/routes/connects/+page.svelte
@@ -0,0 +1,20 @@
+<div class="flex justify-center items-center h-screen bg-gray-100">
+	<div class="text-center">
+		<div class="flex flex-col justify-center gap-4">
+			<a
+				class="px-4 py-2 border w-full border-gray-300 rounded hover:bg-gray-200"
+				href="/api/auth/spotify/login"
+				rel="noopener noreferrer"
+			>
+				Connect Spotify
+			</a>
+			<a
+				class="px-4 py-2 border w-full border-gray-300 rounded hover:bg-gray-200"
+				href="/api/auth/google/login"
+				rel="noopener noreferrer"
+			>
+				Connect Youtube Music
+			</a>
+		</div>
+	</div>
+</div>
